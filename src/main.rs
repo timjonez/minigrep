843fb7472d4197f376220657dfeb70ab1e31ba96
Tim Jones <timjonez@protonmail.com>
2022-01-21 12:26:07 -0600

    [src/main] adjust main logic calling file logic to handle errors

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 struct Config {
