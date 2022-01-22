42001bb274e08bce05ee646b2f426673a2448e49
Tim Jones <timjonez@protonmail.com>
2022-01-22 15:50:36 -0600

    [src/main] reformat print output

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    println!("Searching for \"{}\" in {}", config.query, config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
