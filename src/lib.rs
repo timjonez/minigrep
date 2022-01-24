a878c04b8d570179cea6fe5e17488765a8a4890d
Tim Jones <timjonez@protonmail.com>
2022-01-24 16:08:54 -0600

    [src/lib] adjust the config method to use clap

--- src/lib.rs
+++ src/lib.rs
@@ -20,16 +20,13 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
+    pub fn new() -> Config {
+        let mut config = Config::parse();
+        if !config.ignore_case {
+            config.ignore_case = !env::var("CASE_INSENSITIVE").is_err();
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
 
-        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
-        Ok(Config { query, filename, case_sensitive, })
+        config
     }
 }
 
