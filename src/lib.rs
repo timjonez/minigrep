a3ee03e82ce928f1d0f8ae3167b84abce090c5ac
Tim Jones <timjonez@protonmail.com>
2022-01-24 16:10:04 -0600

    [src/lib] update the config field name ref, and swap bool logic

--- src/lib.rs
+++ src/lib.rs
@@ -33,10 +33,10 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    let results = if config.case_sensitive {
-        search(&config.query, &contents)
-    } else {
+    let results = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
     };
 
     for line in results {
